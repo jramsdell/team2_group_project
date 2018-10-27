e5c15f7cf2d720d95aa3939adbfa1babbded0adc
jramsdell <ramsdell.jordan@gmail.com>
2018-10-27 10:42:57 -0400

    Initial commit

---
+++ src/main/java/edu/unh/cs753/Main.java
@@ -0,0 +1,22 @@
+package edu.unh.cs753;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+
+public class Main {
+
+	// Main class for project
+	public static void main(String[] args) throws IOException {
+		System.setProperty("file.encoding", "UTF-8");
+
+		String option = args[0];
+		String path = args[1];
+
+
+	}
+
+}
