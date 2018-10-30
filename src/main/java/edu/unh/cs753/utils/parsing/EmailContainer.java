953635a922a53512e166244f9d7b25e0833c1e96
tsward6 <35506872+tsward6@users.noreply.github.com>
2018-10-30 18:30:27 -0400

    Update EmailContainer.java
    added list of stings "tokens"

--- src/main/java/edu/unh/cs753/utils/parsing/EmailContainer.java
+++ src/main/java/edu/unh/cs753/utils/parsing/EmailContainer.java
@@ -1,12 +1,16 @@
 package edu.unh.cs753.utils.parsing;
 
+import java.util.List;
+
 public class EmailContainer {
         public String subject;
         public String content;
+        public List<String> tokens;
 
-        public EmailContainer(String sub, String cont, String label) {
+        public EmailContainer(String sub, String cont, String label, List<String> tokens) {
                 subject = sub;
                 content = cont;
+                tokens = tokens;
 
         }
 }
