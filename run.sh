6a22a82621ae32f8f44cb0fe5659139d16d8cb72
tsward6 <35506872+tsward6@users.noreply.github.com>
2018-10-30 19:04:00 -0400

    Create run.sh
    Added a bash script to run the target (maven) jar file

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# maven compile command
+mvn clean compile assembly:single
+if [ $? -eq 1 ]; then
+    echo "Maven compile failed"
+    exit 1
+fi
+
+
+# run the maven jar file (to display results)
+java -jar target/team2_1-1.0-SNAPSHOT-jar-with-dependencies.jar $1 
+if [ $? -eq 1 ]; then
+    echo "Error running jar file"
+    exit 1
+fi
