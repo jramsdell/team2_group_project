5bbfeff0faa7a3b08f2512ea1ef1f2bed8429d7a
tsward6 <35506872+tsward6@users.noreply.github.com>
2018-10-30 18:49:04 -0400

    Update EmailContainer.java
    added filename parameter

--- src/main/java/edu/unh/cs753/utils/parsing/EmailContainer.java
+++ src/main/java/edu/unh/cs753/utils/parsing/EmailContainer.java
@@ -3,14 +3,15 @@ package edu.unh.cs753.utils.parsing;
 import java.util.List;
 
 public class EmailContainer {
+        public String filename;
         public String subject;
         public String content;
         public List<String> tokens;
 
-        public EmailContainer(String sub, String cont, String label, List<String> tokens) {
+        public EmailContainer(String filename, String sub, String cont, String label, List<String> tokens) {
+                filename = filename;
                 subject = sub;
                 content = cont;
                 tokens = tokens;
-
         }
 }
